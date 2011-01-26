25b3db89cc23e2c5e09012ed10aca6b84337e5e1
Daniel Pfeifer <daniel@pfeifer-mail.de>
2011-01-26 14:12:39 +0100

    silence some warnings

--- src/CMake/BoostFetchPristineBoost.cmake
+++ src/CMake/BoostFetchPristineBoost.cmake
@@ -1,25 +1,27 @@
 
-include(BoostFetchDownload)
+set(PRISTINE_BOOST "$ENV{PRISTINE_BOOST}")
 
 # all modules that are not yet modularized are fetched from this mirror
-boost_fetch_download(pristine_boost ${CMAKE_BINARY_DIR}
-  URL http://sourceforge.net/projects/boost/files/boost/1.45.0/boost_1_45_0.tar.gz/download
-  MD5 739792c98fafb95e7a6b5da23a30062c
-  )
+if(NOT EXISTS "${PRISTINE_BOOST}/LICENSE_1_0.txt")
+  include(BoostFetchDownload)
+  boost_fetch_download(pristine_boost ${CMAKE_BINARY_DIR}
+    URL http://sourceforge.net/projects/boost/files/boost/1.45.0/boost_1_45_0.tar.gz/download
+    MD5 739792c98fafb95e7a6b5da23a30062c
+    )
+  set(PRISTINE_BOOST "${CMAKE_BINARY_DIR}/pristine_boost")
+endif()
 
 function(boost_fetch_pristine_boost name destination)
-  set(pristine_boost_dir "${CMAKE_BINARY_DIR}/pristine_boost")
-  
   if(BOOST_${name}_TOOL)
-    set(srcroot "${pristine_boost_dir}/tools")
+    set(srcroot "${PRISTINE_BOOST}/tools")
   else()
-    set(srcroot "${pristine_boost_dir}/libs")
+    set(srcroot "${PRISTINE_BOOST}/libs")
   endif()
 
   file(COPY "${srcroot}/${name}" DESTINATION "${destination}")
 
   foreach(header ${ARGN})
     string(REGEX MATCH "(.*)[/\\]" dir "${header}")
-    file(COPY "${pristine_boost_dir}/${header}" DESTINATION "${destination}/${name}/${dir}")
+    file(COPY "${PRISTINE_BOOST}/${header}" DESTINATION "${destination}/${name}/${dir}")
   endforeach(header)
 endfunction(boost_fetch_pristine_boost)
