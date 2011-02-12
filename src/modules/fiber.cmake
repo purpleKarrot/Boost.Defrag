2514a6f4ae94c6cc8bc356f4db19021b7212569f
Daniel Larimer <dlarimer@gmail.com>
2011-02-12 17:01:35 -0500

    I did a merge wrong, these files were moved

--- src/modules/fiber.cmake
+++
@@ -1,7 +0,0 @@
-# Distributed under the Boost Software License, Version 1.0.
-# See http://www.boost.org/LICENSE_1_0.txt
-
-boost_module(fiber
-    GIT
-        URL git://github.com/bytemaster/boost_fiber.git
-  )
