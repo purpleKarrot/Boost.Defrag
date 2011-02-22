f88040a5266f545e106f38836e8fbbe93c1fc9f3
Daniel Pfeifer <daniel@pfeifer-mail.de>
2011-02-22 08:50:02 +0100

    switch to Boost 1.46

--- CMake/BoostFetchPristineBoost.cmake
+++ CMake/BoostFetchPristineBoost.cmake
@@ -5,8 +5,8 @@ set(PRISTINE_BOOST "$ENV{PRISTINE_BOOST}")
 if(NOT EXISTS "${PRISTINE_BOOST}/LICENSE_1_0.txt")
   include(BoostFetchDownload)
   boost_fetch_download(pristine_boost ${CMAKE_BINARY_DIR}
-    URL http://sourceforge.net/projects/boost/files/boost/1.45.0/boost_1_45_0.tar.gz/download
-    MD5 739792c98fafb95e7a6b5da23a30062c
+    URL http://sourceforge.net/projects/boost/files/boost/1.46.0/boost_1_46_0.tar.bz2/download
+    MD5 37b12f1702319b73876b0097982087e0
     )
   set(PRISTINE_BOOST "${CMAKE_BINARY_DIR}/pristine_boost")
 endif()
