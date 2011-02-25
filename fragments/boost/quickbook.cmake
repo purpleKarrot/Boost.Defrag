d6ae7820b11bc9a1599c4eb3528d500410749144
Daniel Pfeifer <daniel@pfeifer-mail.de>
2011-02-25 22:02:41 +0100

    updated quickbook version

--- fragments/boost/quickbook.cmake
+++ fragments/boost/quickbook.cmake
@@ -3,7 +3,10 @@
 
 boost_module(quickbook
   STABLE SVN
-    URL http://svn.boost.org/svn/boost/tags/release/Boost_1_46_0/tools/quickbook/
+#   URL http://svn.boost.org/svn/boost/tags/release/Boost_1_46_0/tools/quickbook/
+    URL http://svn.boost.org/svn/boost/trunk/tools/quickbook/
+    REV 69249 # Workaround for optimization bug in 64-bit g++ 4.4.
   UNSTABLE SVN
     URL http://svn.boost.org/svn/boost/trunk/tools/quickbook/
+    REV HEAD
   )
